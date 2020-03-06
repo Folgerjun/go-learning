f81bf1f71e7258c6007d6dcae9361271e49217a4
Folgerjun <ffj0721@163.com>
2020-03-06 16:53:56 +0800

    defer

---
+++ 20200306/defer.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("defer begin")
+
+	// 将 defer 放入延迟调用栈
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	// 最后一个放入 位于栈顶 最先调用
+	defer fmt.Println(3)
+
+	fmt.Println("defer end")
+
+	/*
+		defer begin
+		defer end
+		3
+		2
+		1
+	*/
+}
