9c9dfed2cced6dd199db8d90fd961a778a443882
Folgerjun <ffj0721@163.com>
2020-03-10 13:28:28 +0800

    二维矢量模拟玩家移动

---
+++ 20200310/player/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 实例化玩家对象 并设速度为 0.5
+	p := NewPlayer(0.5)
+	// 让玩家移动到 3,1 点
+	p.MoveTo(Vec2{3, 1})
+
+	// 如果没有到达就一直循环
+	for !p.IsArrived() {
+		// 更新玩家位置
+		p.Update()
+		// 打印每次移动后的玩家位置
+		fmt.Println(p.Pos())
+	}
+}
