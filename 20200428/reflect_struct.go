7f685d7071bfb0b757cb6b680de7838cd4439983
Folgerjun <ffj0721@163.com>
2020-04-28 11:50:51 +0800

    反射 结构体成员 标签

---
+++ 20200428/reflect_struct.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+
+	// 声明结构体
+	type cat struct {
+		Name string
+		// 带有结构体 tag 的字段
+		Type int `json:"type" id:"100"`
+	}
+	// 创建 cat 的实例
+	ins := cat{Name: "ffj", Type: 1}
+	// 获取结构体实例的反射类型对象
+	typeOfCat := reflect.TypeOf(ins)
+	// 遍历结构体所有成员
+	for i := 0; i < typeOfCat.NumField(); i++ {
+		// 获取每个成员的结构体字段类型
+		fieldType := typeOfCat.Field(i)
+		// 输出成员名和 tag
+		fmt.Printf("name: '%v'  tag: '%v'\n", fieldType.Name, fieldType.Tag)
+	}
+	// 通过字段名 找到字段类型信息
+	if catType, ok := typeOfCat.FieldByName("Type"); ok {
+		// 从 tag 中取出需要的 tag
+		fmt.Println(catType.Tag.Get("json"), catType.Tag.Get("id"))
+	}
+
+	/*
+		name: 'Name'  tag: ''
+		name: 'Type'  tag: 'json:"type" id:"100"'
+		type 100
+	*/
+}
